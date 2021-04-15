94863d843d4396e48420806c946f2600c97ea0ae
Daniel Newman <dwnewman78@gmail.com>
2021-04-15 15:48:37 -0500

    Comment out installing dotfiles for Mac setup

--- mac/get_xoi_version.sh
+++
@@ -1,7 +0,0 @@
-VERSION=$(cat xoi-version.json| jq -r '.version')
-IFS='.' read -r -a SPLIT_VERSION <<< "$VERSION"
-MAJOR="$((${SPLIT_VERSION[0]} * 1000000))"
-MINOR="$((${SPLIT_VERSION[1]} * 1000))"
-PATCH="${SPLIT_VERSION[2]}"
-SEQUENCE="${MAJOR}${MINOR}${PATCH}"
-echo $SEQUENCE
